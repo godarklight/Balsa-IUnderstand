ff7551d23b97ea93c409f34f9ba2964670d9a075
Christopher Andrews <godarklight@gmail.com>
2021-09-02 18:02:03 +1000

    Initial commit

---
+++ IUnderstand/IUnderstandMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UI;
+
+namespace IUnderstand
+{
+    public class IUnderstandMain : MonoBehaviour
+    {
+        int frameCountdown = 50;
+
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+            if (frameCountdown > 0)
+            {
+                frameCountdown--;
+                if (frameCountdown == 0)
+                {
+                    GameObject[] gos = GameObject.FindObjectsOfType<GameObject>();
+                    GameObject goButton = null;
+                    foreach (GameObject go in gos)
+                    {
+                        if (go.name == "button_template(Clone)")
+                        {
+                            goButton = go;
+                        }
+                    }
+                    goButton?.GetComponent<UIButton>()?.onClick.Invoke();
+                    Destroy(this);
+                }
+            }
+        }
+    }
+}
---
+++ IUnderstand/Loader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BalsaCore;
+using UnityEngine;
+
+namespace IUnderstand
+{
+    [BalsaAddon]
+    public class Loader
+    {
+        private static bool loaded = false;
+        private static GameObject go;
+        private static MonoBehaviour mod;
+
+        //Game start
+        [BalsaAddonInit]
+        public static void BalsaInit()
+        {
+            if (!loaded)
+            {
+                loaded = true;
+                go = new GameObject();
+                mod = go.AddComponent<IUnderstandMain>();
+            }
+        }
+
+        //Game exit
+        [BalsaAddonFinalize]
+        public static void BalsaFinalize()
+        {
+        }
+    }
+}
